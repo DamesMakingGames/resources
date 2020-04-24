26f84418e0c38de3e17a9f8a6f7bb20e51b6f466
Jennie Robinson Faber <jennie@dmg.to>
2020-04-23 20:42:50 -0400

    Update 404 page svg error

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,18 +1,18 @@
 <template>
   <Layout>
-    <div class="container-inner mx-auto py-16">
-      <h2 class="text-4xl font-bold mb-16">Page Not Found</h2>
-      <g-image src="../../static/404.svg" />
+    <div class="container-inner text-center mx-auto p-12 pt-0 md:w-2/3">
+      <h2 class="text-3xl mb-4">Page Not Found</h2>
+      <p class="text-red-600 font-bold text-xl my-4 leading-tight">
+        Sorry! We couldn't find a page at "{{ $route.path }}".
+      </p>
     </div>
-
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Page Not Found'
-  }
-}
+    title: "Page Not Found",
+  },
+};
 </script>
-
