f86ed2defb92f2403fcdd75b3b8a84d2834db2d9
Jennie Robinson Faber <jennie@dmg.to>
2020-04-29 10:51:13 -0400

    Add text intro to reading lists overview page

--- src/templates/ReadingLists.vue
+++ src/templates/ReadingLists.vue
@@ -2,7 +2,40 @@
   <Layout>
     <div class="container-inner mx-auto py-16">
       <h1 class="text-4xl font-bold leading-tight">Reading Lists</h1>
-
+      <div class="intro mt-6 p-6 bg-gray-100">
+        <p marked>
+          We&#8217;re compiling read/watch recommendations on topics related to
+          <b>
+            everything making games
+          </b>
+          from members of our community. Want to contribute a curated list of
+          books, links, videos or other resources on a topic?
+        </p>
+        <span class="lg:m-0 lg:p-0 align-middle my-6 flex">
+          <a
+            class="btn btn-red leading-normal align-middle flex"
+            href="mailto:membership@dmg.to"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-mail mr-3"
+            >
+              <path
+                d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
+              ></path>
+              <polyline points="22,6 12,13 2,6"></polyline>
+            </svg>
+            Email us</a
+          >
+        </span>
+      </div>
       <div class="">
         <div
           v-for="list in $page.lists.edges"
